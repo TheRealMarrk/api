951106d1d6ad9b5ff9b868b8c33cf5da2182f76f
Matheus Carvalho <matheuscarvalho98@outlook.com.br>
2022-12-26 11:21:20 -0300

    initial commit

---
+++ src/routes/users.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express")
+
+const UsersController = require("../controllers/UsersController")
+
+const usersRoutes = Router()
+
+const usersController = new UsersController()
+
+
+
+usersRoutes.post("/users", usersController.create)
+usersRoutes.put("/users/:id", usersController.update)
+
+module.exports = usersRoutes
